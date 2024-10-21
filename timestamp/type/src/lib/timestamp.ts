1eafa25618a35daa57fc0887f2e093eea9a2e5e9
Artur Gubaidullin <lapteuh@gmail.com>
2024-10-21 10:44:42 +0000

    Add timestamp type

---
+++ timestamp/type/src/lib/timestamp.ts
@@ -0,0 +1,16 @@
+export interface SimplifiedTimestamp {
+  readonly seconds: number;
+  readonly nanoseconds: number;
+}
+
+export interface Timestamp {
+  readonly seconds: number;
+  readonly nanoseconds: number;
+
+  readonly toDate: () => Date;
+  readonly toMillis: () => number;
+  readonly isEqual: (other: Timestamp) => boolean;
+  readonly toString: () => string;
+  readonly valueOf: () => string;
+  readonly toJSON: () => SimplifiedTimestamp;
+}
