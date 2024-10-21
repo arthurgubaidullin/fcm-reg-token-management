c84d1566651b3696465a4354b3fb33cbb0ffa519
Artur Gubaidullin <lapteuh@gmail.com>
2024-10-21 18:47:36 +0000

    Move & rename lib

--- reg-token-set/default/jest.config.ts
+++ reg-token-set/default/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'default-user-reg-token-set',
+  displayName: 'default-reg-token-set',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/user-reg-token-set/default',
+  coverageDirectory: '../../coverage/reg-token-set/default',
 };
