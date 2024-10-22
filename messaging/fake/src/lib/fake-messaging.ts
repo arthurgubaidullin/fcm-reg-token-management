0ef7adf010f21bf11b886004a4e77a69aca818b5
Artur Gubaidullin <lapteuh@gmail.com>
2024-10-22 12:12:08 +0000

    Fix type

--- messaging/fake/src/lib/fake-messaging.ts
+++ messaging/fake/src/lib/fake-messaging.ts
@@ -1,10 +1,10 @@
+import { DefaultDeviceId } from '@arthurgubaidullin/default-device-id';
 import { DefaultRegToken } from '@arthurgubaidullin/default-reg-token';
 import { Timestamp } from '@arthurgubaidullin/default-timestamp';
 import { Messaging } from '@arthurgubaidullin/messaging';
-import { DefaultDeviceId } from '@arthurgubaidullin/default-device-id';
 
 export class FakeMessaging implements Messaging {
-  getToken() {
+  async getToken() {
     return new DefaultRegToken(
       'test',
       new DefaultDeviceId('test'),
--- messaging/type/src/lib/messaging.ts
+++ messaging/type/src/lib/messaging.ts
@@ -1,5 +1,6 @@
 import { RegToken } from '@arthurgubaidullin/reg-token';
+import { Timestamp } from '@arthurgubaidullin/timestamp';
 
 export interface Messaging {
-  readonly getToken: () => RegToken;
+  readonly getToken: (now: Timestamp) => Promise<RegToken>;
 }
