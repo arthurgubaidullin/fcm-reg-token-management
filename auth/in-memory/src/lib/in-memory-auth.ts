9a5ca6b854df903c6cf15cbaaded913152fc4562
Artur Gubaidullin <lapteuh@gmail.com>
2024-10-22 19:05:08 +0000

    Remove lib

--- auth/in-memory/src/lib/in-memory-auth.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultUser, DefaultUserId } from '@arthurgubaidullin/default-user';
-import { User } from '@arthurgubaidullin/user';
-import { Auth } from '@arthurgubaidullin/auth-type';
-
-export class InMemoryAuth implements Auth {
-  get currentUser(): User | null {
-    return new DefaultUser(new DefaultUserId('test'));
-  }
-}
