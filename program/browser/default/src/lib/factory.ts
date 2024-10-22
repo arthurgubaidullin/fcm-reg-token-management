03a2ab7b21d3d0c62d5949483684934094843b88
Artur Gubaidullin <lapteuh@gmail.com>
2024-10-22 11:49:38 +0000

    Add auth to browser program

--- program/browser/default/src/lib/factory.ts
+++ program/browser/default/src/lib/factory.ts
@@ -1,11 +1,16 @@
+import { getAuth } from 'firebase/auth';
 import { DefaultBrowserProgram } from './program';
+import { FirebaseApp } from 'firebase/app';
+import { FirebaseAuthAdapter } from '@arthurgubaidullin/firebase-auth-adapter';
 
 let instance: DefaultBrowserProgram | null = null;
 
 export class DefaultBrouserPropramFactory {
-  static default() {
+  static default(firebaseApp: FirebaseApp) {
     if (instance === null) {
-      instance = new DefaultBrowserProgram();
+      const auth = new FirebaseAuthAdapter(getAuth(firebaseApp));
+
+      instance = new DefaultBrowserProgram(auth);
     }
     return instance;
   }
--- program/browser/default/src/lib/program.ts
+++ program/browser/default/src/lib/program.ts
@@ -1,6 +1,9 @@
 import { BrowserProgram } from '@arthurgubaidullin/browser-program';
+import { Auth } from '@arthurgubaidullin/auth';
 
 export class DefaultBrowserProgram implements BrowserProgram {
+  constructor(private readonly auth: Auth) {}
+
   start() {
     return () => this.stop();
   }
