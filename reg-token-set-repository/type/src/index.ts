a7f6eb0cfb42d3ba658cbbfd737d5f6846db2d9e
Artur Gubaidullin <lapteuh@gmail.com>
2024-10-22 18:26:08 +0000

    Remove lib

--- reg-token-set-repository/type/src/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './lib/reg-token-set-repository';
--- reg-token-set-repository/type/src/lib/reg-token-set-repository.ts
+++
@@ -1,4 +0,0 @@
-export interface RegTokenSetRepository {
-  readonly get: (id: string) => Record<string, unknown> | undefined;
-  readonly set: (id: string, data: Record<string, unknown>) => void;
-}
