f5aaac2f429230dcec46c5f717100959762a1548
Artur Gubaidullin <lapteuh@gmail.com>
2024-10-22 16:30:07 +0000

    Fix web-app

--- examples/web-app/src/app/app.tsx
+++ examples/web-app/src/app/app.tsx
@@ -1,4 +1,24 @@
+import * as firebase from 'firebase/app';
+import { DefaultBrouserPropramFactory } from '@arthurgubaidullin/default-browser-program';
+import { useEffect } from 'react';
+
+const firebaseApp = firebase.initializeApp({
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+});
+
+const program = DefaultBrouserPropramFactory.default(firebaseApp);
+
 export function App() {
+  useEffect(() => {
+    return program.start();
+  }, []);
+
   return <div>test</div>;
 }
 
