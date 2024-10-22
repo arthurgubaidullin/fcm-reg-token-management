89f7d8153e8655520df0c05f398532d373555ba4
Artur Gubaidullin <lapteuh@gmail.com>
2024-10-22 11:49:08 +0000

    Add firebase auth adapter

---
+++ auth/firebase-adapter/src/lib/firebase-auth.ts
@@ -0,0 +1,23 @@
+import { Auth } from '@arthurgubaidullin/auth';
+import { DefaultUser, DefaultUserId } from '@arthurgubaidullin/default-user';
+import * as FirbaseAuth from 'firebase/auth';
+
+export class FirebaseAuthAdapter implements Auth {
+  private auth: FirbaseAuth.Auth;
+
+  constructor(firebaseAuth: FirbaseAuth.Auth) {
+    this.auth = firebaseAuth;
+  }
+
+  get currentUser() {
+    const user = this.auth.currentUser;
+
+    if (!user) {
+      return null;
+    }
+
+    const id = new DefaultUserId(user.uid);
+
+    return new DefaultUser(id);
+  }
+}
