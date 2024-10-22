956bac57479a5409859c5f31951d462c352d76e0
Artur Gubaidullin <lapteuh@gmail.com>
2024-10-22 12:17:23 +0000

    Add messaging to browser program

--- program/browser/default/src/lib/factory.ts
+++ program/browser/default/src/lib/factory.ts
@@ -2,6 +2,9 @@ import { getAuth } from 'firebase/auth';
 import { DefaultBrowserProgram } from './program';
 import { FirebaseApp } from 'firebase/app';
 import { FirebaseAuthAdapter } from '@arthurgubaidullin/firebase-auth-adapter';
+import { FirebaseMessagingAdapter } from '@arthurgubaidullin/firebase-messaging-adapter';
+import { getInstallations } from 'firebase/installations';
+import { getMessaging } from 'firebase/messaging';
 
 let instance: DefaultBrowserProgram | null = null;
 
@@ -9,8 +12,12 @@ export class DefaultBrouserPropramFactory {
   static default(firebaseApp: FirebaseApp) {
     if (instance === null) {
       const auth = new FirebaseAuthAdapter(getAuth(firebaseApp));
+      const messaging = new FirebaseMessagingAdapter(
+        getMessaging(firebaseApp),
+        getInstallations(firebaseApp)
+      );
 
-      instance = new DefaultBrowserProgram(auth);
+      instance = new DefaultBrowserProgram(auth, messaging);
     }
     return instance;
   }
--- program/browser/default/src/lib/program.ts
+++ program/browser/default/src/lib/program.ts
@@ -1,8 +1,12 @@
 import { BrowserProgram } from '@arthurgubaidullin/browser-program';
 import { Auth } from '@arthurgubaidullin/auth';
+import { Messaging } from '@arthurgubaidullin/messaging';
 
 export class DefaultBrowserProgram implements BrowserProgram {
-  constructor(private readonly auth: Auth) {}
+  constructor(
+    private readonly auth: Auth,
+    private readonly messaging: Messaging
+  ) {}
 
   start() {
     return () => this.stop();
